d613fa627a925a848ac5191125d3a4e0b5b96b83
Vini Oliveira <vinicius.oliveira@warrenbrasil.com.br>
2022-05-10 16:11:30 -0300

    feat: Custom Json Converter  for DateTime

---
+++ ProjetoWarren.WebAPI/Serialization/CustomDateTimeConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ProjetoWarren.WebAPI.Serialization
+{
+    public class CustomDateTimeConverter : JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeToConvert == typeof(DateTime);
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            try
+            {
+                var converter = ConverterFactory(typeof(DateTimeConverter), typeToConvert);
+
+                return converter;
+            }
+            catch (TargetInvocationException targetInvocationEx)
+            {
+                if (targetInvocationEx.InnerException != null) throw targetInvocationEx.InnerException;
+
+                throw;
+            }
+        }
+
+        private static JsonConverter ConverterFactory(Type converter, Type typeToConvert)
+        {
+            var jsonConverter = (JsonConverter)Activator.CreateInstance(
+                converter.MakeGenericType(typeToConvert),
+                BindingFlags.Instance | BindingFlags.Public,
+                binder: null,
+                args: Array.Empty<object>(),
+                culture: null
+            )!;
+
+            return jsonConverter;
+        }
+    }
+}
---
+++ ProjetoWarren.WebAPI/Serialization/DateTimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ProjetoWarren.WebAPI.Serialization
+{
+    public class DateTimeConverter<DateTime> : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var tokenType = reader.TokenType;
+            var dateTimeText = reader.GetString();
+
+            if (tokenType != JsonTokenType.String)
+            {
+                throw new NotSupportedException($"Value {dateTimeText} not supported.");
+            }
+            
+            if (DateTime.TryParse(dateTimeText, new CultureInfo("pt-BR"), DateTimeStyles.None, out DateTime date)) return date;
+            throw new NotSupportedException($"Value {dateTimeText} not supported.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
