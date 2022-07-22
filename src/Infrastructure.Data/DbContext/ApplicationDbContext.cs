2a24a15e7fd92265e8f39d1234e6ce1a13400f47
Claudinei Santos <claudinei.santos@warren.com.br>
2022-07-22 14:16:00 -0300

    refactor: configuracoes no contexto para utilizar UnitOfWork

--- src/Infrastructure.Data/DbContext/ApplicationDbContext.cs
+++ src/Infrastructure.Data/DbContext/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using DomainModels;
+using EntityFrameworkCore.AutoHistory.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
@@ -6,10 +7,29 @@ namespace Infrastructure.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+    {
+        ChangeTracker.LazyLoadingEnabled = false;
+        ChangeTracker.AutoDetectChangesEnabled = false;
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+    }
+
+    public override int SaveChanges()
+    {
+        var addedEntities = this.DetectChanges(EntityState.Added);
+
+        this.EnsureAutoHistory();
+        var affectedRows = base.SaveChanges();
+
+        this.EnsureAutoHistory(addedEntities);
+        affectedRows += base.SaveChanges();
+
+        return affectedRows;
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.EnableAutoHistory();
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.Load("Infrastructure.Data"));
     }
 
