31526ad00f7301bf7809f9ff1c6af2b1cd0a827e
Claudinei Santos <claudinei.santos@warren.com.br>
2022-07-22 21:29:18 -0300

    refactor: criacao de repository factory

---
+++ src/DomainServices/GenericRepositories/Interface/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DomainServices.GenericRepositories.Interface;
+
+public interface IGenericRepository<TEntity> where TEntity : class
+{
+    bool Insert(TEntity obj);
+    IEnumerable<TEntity> GetAll();
+    TEntity GetById(object id);
+    IQueryable GetByName(object fullName);
+    bool Update(TEntity obj);
+    bool Delete(object obj);
+}
