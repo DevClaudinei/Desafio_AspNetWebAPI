f5d18012e1db7d2744c523f185976a1bcf44bd68
Claudinei Santos <claudinei.santos@gwarren.com.br>
2022-08-07 13:30:50 -0300

    refactor: insercao de configuracao para services da nova entidade

--- src/API/Configurations/ServicesConfiguration.cs
+++ src/API/Configurations/ServicesConfiguration.cs
@@ -1,6 +1,7 @@
 using AppServices.Services;
 using DomainServices;
 using DomainServices.Services;
+using DomainServices.Services.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +13,8 @@ public static class ServicesConfiguration
     public static void AddServicesConfiguration(this IServiceCollection services)
     {
         services.AddTransient<ICustomerService, CustomerService>();
+
+        services.AddTransient<ICustomerBankInfoService, CustomerBankInfoService>();
 
         services.AddTransient<ICustomerAppService, CustomerAppService>();
 
