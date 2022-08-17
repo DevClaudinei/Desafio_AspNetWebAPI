db2c8d3109fc4ce92fa9c006a02150a40df7ed0f
Claudinei Santos <claudinei.santos@warren.com.br>
2022-08-17 03:37:02 -0300

    refactor: criacao de um profile para portfolio

---
+++ src/AppServices/Profiles/PortfolioProfile.cs
@@ -0,0 +1,16 @@
+﻿using Application.Models.Portfolio.Request;
+using Application.Models.Portfolio.Response;
+using Application.Models.Product.Response;
+using AutoMapper;
+using DomainModels.Entities;
+
+namespace AppServices.Profiles;
+
+public class PortfolioProfile : Profile
+{
+	public PortfolioProfile()
+	{
+        CreateMap<Portfolio, PortfolioResult>();
+        CreateMap<PortfolioCreate, Portfolio>();
+    }
+}
