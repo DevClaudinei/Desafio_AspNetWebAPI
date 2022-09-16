355cd160fe00828469380b3a022e083cce65021f
Claudinei Santos <claudinei.santos@warren.com.br>
2022-09-16 15:50:24 -0300

    refactor: centralizacao das injecoes de dependencias

---
+++ src/API/Configurations/ServicesConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using AppServices.Services;
+using AppServices.Services.Interfaces;
+using DomainServices;
+using DomainServices.Services;
+using DomainServices.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Configurations;
+
+public static class ServicesConfiguration
+{
+    public static void AddServicesConfiguration(this IServiceCollection services)
+    {
+        services.AddTransient<ICustomerService, CustomerService>();
+
+        services.AddTransient<ICustomerAppService, CustomerAppService>();
+
+        services.AddTransient<ICustomerBankInfoService, CustomerBankInfoService>();
+
+        services.AddTransient<ICustomerBankInfoAppService, CustomerBankInfoAppService>();
+
+        services.AddTransient<IProductService, ProductService>();
+
+        services.AddTransient<IProductAppService, ProductAppService>();
+
+        services.AddTransient<IPortfolioService, PortfolioService>();
+
+        services.AddTransient<IPortfolioAppService, PortfolioAppService>();
+
+        services.AddTransient<IPortfolioProductService, PortfolioProductService>();
+
+        services.AddTransient<IPortfolioProductAppService, PortfolioProductAppService>();
+    }
+}
