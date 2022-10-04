ef60b1bc58da19c4579dff9fca122b1dd2e25084
Claudinei Santos <claudinei.santos@warren.com.br>
2022-10-04 13:07:17 -0300

    refactor: insercao da propriedade direction em order mapping

--- src/Infrastructure.Data/Mappings/OrderMapping.cs
+++ src/Infrastructure.Data/Mappings/OrderMapping.cs
@@ -24,5 +24,8 @@ public class OrderMapping : IEntityTypeConfiguration<Order>
 
         builder.Property(x => x.ConvertedAt)
             .IsRequired();
+
+        builder.Property(x => x.Direction)
+            .IsRequired();
     }
 }
