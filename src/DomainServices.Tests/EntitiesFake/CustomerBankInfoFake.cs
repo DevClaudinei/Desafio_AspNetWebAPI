fcd4475ce62df01891ea965a199a4f65607824b6
Claudinei Santos <claudinei.santos@warren.com.br>
2022-10-25 22:12:00 -0300

    feat: criacao de entidades fakes para utilizar nos testes unitarios

---
+++ src/DomainServices.Tests/EntitiesFake/CustomerBankInfoFake.cs
@@ -0,0 +1,35 @@
+﻿using Bogus;
+using DomainModels.Entities;
+
+namespace DomainServices.Tests.EntitiesFake;
+
+public static class CustomerBankInfoFake
+{
+	public static CustomerBankInfo CustomerBankInfoFaker()
+	{
+		var customerBankInfoFake = new Faker<CustomerBankInfo>("pt_BR")
+			.CustomInstantiator(f => new CustomerBankInfo(customerId: 1)).Generate();
+
+		customerBankInfoFake.Account = Guid.NewGuid().ToString();
+		customerBankInfoFake.AccountBalance = 0;
+		customerBankInfoFake.Id = 1;
+
+		return customerBankInfoFake;
+    }
+
+	public static IEnumerable<CustomerBankInfo> CustomerBankInfoFakers(int quantity)
+	{
+        var id = 1;
+        var customerBankInfoFakes = new Faker<CustomerBankInfo>("pt_BR")
+            .CustomInstantiator(f => new CustomerBankInfo(customerId: 1)).Generate(quantity);
+
+        foreach (var customerBankInfo in customerBankInfoFakes)
+        {
+            customerBankInfo.Account = Guid.NewGuid().ToString();
+			customerBankInfo.AccountBalance = 0;
+            customerBankInfo.Id = id++;
+        }
+
+		return customerBankInfoFakes;
+    }
+}
---
+++ src/DomainServices.Tests/EntitiesFake/OrderFake.cs
@@ -0,0 +1,42 @@
+﻿using Bogus;
+using DomainModels.Entities;
+
+namespace DomainServices.Tests.EntitiesFake;
+
+public static class OrderFake
+{
+	public static Order OrderFaker()
+	{
+		var orderFake = new Faker<Order>("pt_BR")
+			.CustomInstantiator(f => new Order(
+				f.Random.Int(1),
+                f.Random.Int(1)
+            )).Generate();
+
+		orderFake.Id = 1;
+		orderFake.NetValue = 1;
+		orderFake.Quotes = 1;
+
+		return orderFake;
+	}
+
+	public static IEnumerable<Order> OrderFakers(int quantity)
+	{
+        var id = 1;
+        var orderFakes = new Faker<Order>("pt_BR")
+            .CustomInstantiator(f => new Order(
+                f.Random.Int(1),
+                f.Random.Int(1)
+            )).Generate(quantity);
+
+		foreach (var orderfake in orderFakes)
+		{
+			orderfake.Id = id++;
+            orderfake.NetValue = 1;
+            orderfake.Quotes = 1;
+            orderfake.Direction = DomainModels.Enum.OrderDirection.Buy;
+        }
+
+		return orderFakes;
+    }
+}
