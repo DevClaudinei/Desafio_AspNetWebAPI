e9e043906b996f51c976b4f609fd14d96b3c10f9
Claudinei Santos <claudinei.santos@warren.com.br>
2022-11-10 16:33:49 -0300

    feat: criacao de dtos e validators para univestment

---
+++ src/AppServices.Tests/ModelsFake/Portfolio/CreateUninvestmentModel.cs
@@ -0,0 +1,36 @@
+﻿using Application.Models.Enum;
+using Application.Models.Portfolio.Request;
+using Bogus;
+
+namespace AppServices.Tests.ModelsFake.Portfolio;
+
+public static class CreateUninvestmentModel
+{
+    public static UninvestimentRequest UninvestmentFake()
+    {
+        var investmentRequest = new Faker<UninvestimentRequest>("pt_BR")
+            .CustomInstantiator(f => new UninvestimentRequest(
+                ProductId: f.Random.Long(1, 1),
+                PortfolioId: f.Random.Long(1, 1),
+                Quotes: f.Random.Int(1, 10),
+                ConvertedAt: DateTime.Now.AddDays(15),
+                Direction: OrderDirection.Sell
+            )).Generate();
+
+        return investmentRequest;
+    }
+
+    public static UninvestimentRequest UninvestmentInvalid()
+    {
+        var investmentRequest = new Faker<UninvestimentRequest>("pt_BR")
+            .CustomInstantiator(f => new UninvestimentRequest(
+                ProductId: f.Random.Long(1, 1),
+                PortfolioId: f.Random.Long(1, 1),
+                Quotes: f.Random.Int(1, 10),
+                ConvertedAt: DateTime.Now.AddDays(-15),
+                Direction: OrderDirection.Sell
+            )).Generate();
+
+        return investmentRequest;
+    }
+}
---
+++ src/AppServices/Validations/Portfolio/UninvestmentCreateValidator.cs
@@ -0,0 +1,35 @@
+﻿using Application.Models.Enum;
+using Application.Models.Portfolio.Request;
+using FluentValidation;
+using System;
+
+namespace AppServices.Validations.Portfolio;
+
+public class UninvestmentCreateValidator : AbstractValidator<UninvestimentRequest>
+{
+    public UninvestmentCreateValidator()
+    {
+        RuleFor(x => x.ProductId)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(x => x.PortfolioId)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(x => x.Quotes)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(x => x.Direction)
+            .NotEmpty()
+            .NotNull()
+            .Equal(OrderDirection.Sell);
+
+        RuleFor(x => x.ConvertedAt)
+            .NotEmpty()
+            .NotNull()
+            .When(x => x.Direction.Equals(OrderDirection.Sell))
+            .GreaterThanOrEqualTo(DateTime.Now);
+    }
+}
